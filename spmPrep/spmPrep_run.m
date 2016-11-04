e2bf071f8444c52c95efd5c0d3f5da2f1dd83b8a
lucas-rosen1 <lrosen27@jhu.edu>
2016-11-04 16:05:03 -0400

    Add files via upload

--- spmPrep/spmPrep_run.m
+++ spmPrep/spmPrep_run.m
@@ -1,4 +1,4 @@
-%%function batchInfo = spmPrep_run(sessionsDirectory,batchOptions)
+function batchInfo = spmPrep_run(sessionsDirectory,batchOptions)
 %% spmPrep_options  Lucas Rosen 10/28/16
 % Sorts fMRI image directories, moves files into correct places for
 % preproccessing, and creates structure with parameters needed to fill the 
@@ -9,7 +9,8 @@
 %   batchOptions -- structure containing info about:
 %       1. template batch files to use for preproccessing
 %       2. files that contain frames with no scans
-%       3. tissue reference file to use for preproccessing
+%       3. whether or not to search all files
+%       4. tissue reference file to use for preproccessing
 %
 % Outputs:
 %   batchInfo - structure containing info neccessary for preproccessing
@@ -39,71 +40,113 @@ sessions={'/Users/lrosen/Documents/Sessions/E/Screening','/Users/lrosen/Document
 batchInfo.sessions=sessions;
 
 %% Part 2 - put batch into working dir 
-% will use varagin/naragin to see if have different batch file although im
-
-%something like if varagin<1
 spmPrepPath=strcat(fileparts(which('spmPrep_run'))); %retrieve spm path
-batchPath=fullfile(spmPrepPath,'batch.mat'); %this is hardcoded but it will always be true
+if ~isfield(batchOptions,'template') %if there is no template use defualt
+    batchPath=fullfile(spmPrepPath,'batch.mat'); 
+    batchInfo.usedDefaulBatch=0;
+else
+    if class(batchOptions.template~='char'
+        disp('Template batch file inputted incorrectly, using default');
+        batchPath=fullfile(spmPrepPath,'batch.mat'); 
+        batchInfo.usedDefaulBatch=0;
+
+    else
+        batchPath=batchOptions.template;
+        batchInfo.usedDefaultBatch=1;
+    end
+end
+
 for i=1:length(sessions);
-    copyfile(batchPath,char(sessions(i))); %move the batch file into the session directories
+        copyfile(batchPath,char(sessions(i))); %move the batch file into the session directories
 end
 disp('Batch files copied');
-batchInfo.usedDefaultBatch=1;
-%code differently if they have their own input - will do later want to work
-%bottom up from simplest case
-%batchInfo.usedDefaulBatch=0;
-
 batchInfo.pathToTemplateBatch=batchPath;
 %% Part 3 - In each session directory, go through and select frames needed for problematic files
 
 %create default structure for fMRI psilocybin studies, if this program is
 %used in the future these lines will likely be modified
-%if nargin<2 or something like this
-badFrames=struct();
-badFrames.searchAll=0; %defines if every single nii file is to be searched
-badFrames.keyWords={'music','test'}; %create cell array of key words to search from
-badFrames.fileNums={};
 
-numBadFiles=0; %defines number of files that have erroneous frames, will be used in making matrix of files and frame numbers
-keyWords=badFrames.keyWords;
-warning('off','all') %scrolling through frames brings up annoying warnings
-if badFrames.searchAll==0 && ~isempty(keyWords) %if we shouldn't search for everything and there are defined key words
-    for i=1:length(sessions) %go through all sessions
-        epiPath=fullfile(char(sessions(i)),'epi');
-            for j=1:length(keyWords); %go through all key words
-                wildCard=strcat(epiPath,'/*',char(keyWords(j)),'*.nii'); %% could be any 4d image format, but I only know of .nii
-                badFiles=dir(wildCard);
-                for k=1:length(badFiles) %% go through all files with erroneous frames
-                    numBadFiles=numBadFiles+1;
-                    fullPathtoBadFile=fullfile(epiPath,badFiles(k).name);
-                    spm_check_registration(fullPathtoBadFile); %display 4d image so that user can scroll through frames, 
-                    % issue with this is that the frame number isn't
-                    % visible so I need to figure out a way to modify the
-                    % graph
-                    
-                    %set up parameters for input box
-                    inpnames='Input';
-                    numlines=1;
-                    defaultanswer={''};
-                    options.WindowStyle='normal';
-                    
-                    %ask user how many relevant frames are in image and add
-                    %to matrix only if they inputed a single number
-                    relFrames = inputdlg(strcat('How many relevant frames are in this image ?'),inpnames,numlines,defaultanswer,options);
-                    relFrames = str2num(relFrames{1});
-                    while isempty(relFrames) || ~isequal(size(relFrames),[1,1])
-                        relFrames = inputdlg(strcat('Please input a single number. How many relevant frames are in this image ?'),inpnames,numlines,defaultanswer,options);
-                        relFrames = str2num(relFrames{1});   
-                    end
-                    %create matrix containing directory, file name, and
-                    %relevant frames for each file that must be checked
-                    badFrames.filenums{1,numBadFiles}=sessions(i);
-                    badFrames.filenums{2,numBadFiles}=badFiles(k).name;
-                    badFrames.filenums{3,numBadFiles}=relFrames;
-                end
-            end
+%check in on user's input of searchAll
+if isfield(batchOptions,'searchAll')
+    if class(batchOptions.searchAll)=='double'
+        if ~(batchOptions.searchAll==0 || batchOptions.searchAll==1)
+            disp('Search all frames inputted incorrectly. Will only search "music"');
+            batchOptions.searchAll=0;
+        end
+    elseif class(batchOptions.searchAll=='char')
+        if str2double(batchOptions.searchAll)==0 || str2double(batchOptions.searchAll)==1
+            batchOptions.searchAll=str2double(batchOptions.searchAll);
+        else
+            disp('Search all frames inputted incorrectly. Will only search "music"');
+            batchOptions.searchAll=0;
+        end
+    else
+        disp('Search all frames inputted incorrectly. Will only search "music"');
+        batchOptions.searchAll=0;
     end
 end
+
+badFrames=struct();
+badFrames.fileNums={};
+
+if ~isfield(batchOptions,'badFrames');
+    badFrames.keywords={'music'};
+else
+    if class(batchOptions.badFrames)=='cell'
+        badFrames.keywords=batchOptions.badFrames';
+    elseif class(batchOptions.badFrames)=='char'
+        badFrames.keywords{1}=batchOptions.badFrames;
+    else
+        disp('Keywords inputted incorrectly, using default "music"');
+        badFrames.keywords={'music'};
+    end
+end
+
+if ~isfield(batchOptions,'searchAll') || (isfield(batchOptions,'searchAll') && batchOptions.searchAll==0)
+    batchOptions.searchedAll=0;
+    numBadFiles=0; %defines number of files that have erroneous frames, will be used in making matrix of files and frame numbers
+    keyWords=badFrames.keyWords;
+    warning('off','all') %scrolling through frames brings up annoying warnings
+    if badFrames.searchAll==0 && ~isempty(keyWords) %if we shouldn't search for everything and there are defined key words
+        for i=1:length(sessions) %go through all sessions
+            epiPath=fullfile(char(sessions(i)),'epi');
+                for j=1:length(keyWords); %go through all key words
+                    wildCard=strcat(epiPath,'/*',char(keyWords(j)),'*.nii'); %% could be any 4d image format, but I only know of .nii
+                    badFiles=dir(wildCard);
+                    for k=1:length(badFiles) %% go through all files with erroneous frames
+                        numBadFiles=numBadFiles+1;
+                        fullPathtoBadFile=fullfile(epiPath,badFiles(k).name);
+                        spm_check_registration(fullPathtoBadFile); %display 4d image so that user can scroll through frames, 
+                        % issue with this is that the frame number isn't
+                        % visible so I need to figure out a way to modify the
+                        % graph
+                    
+                        %set up parameters for input box
+                        inpnames='Input';
+                        numlines=1;
+                        defaultanswer={''};
+                        options.WindowStyle='normal';
+                    
+                        %ask user how many relevant frames are in image and add
+                        %to matrix only if they inputed a single number
+                        relFrames = inputdlg(strcat('How many relevant frames are in this image ?'),inpnames,numlines,defaultanswer,options);
+                        relFrames = str2num(relFrames{1});
+                        while isempty(relFrames) || ~isequal(size(relFrames),[1,1])
+                            relFrames = inputdlg(strcat('Please input a single number. How many relevant frames are in this image ?'),inpnames,numlines,defaultanswer,options);
+                            relFrames = str2num(relFrames{1});   
+                        end
+                        %create matrix containing directory, file name, and
+                        %relevant frames for each file that must be checked
+                        badFrames.filenums{1,numBadFiles}=sessions(i);
+                        badFrames.filenums{2,numBadFiles}=badFiles(k).name;
+                        badFrames.filenums{3,numBadFiles}=relFrames;
+                    end
+                end
+        end
+    end
+elseif (isfield(batchOptions,'searchAll') && batchOptions.searchAll==1)
+    batchOptions.searchedAll=1;
+    %% add commands to search thru all files
 close(gcf);
 batchInfo.badFrames=badFrames;
 warning('on'); %turn warnings back on
